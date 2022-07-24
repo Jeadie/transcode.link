5f4150939a75b72b1bc2d1036a84539ba2b91ab3
jeadi <jeadi@amazon.com>
2022-07-24 21:07:55 +1000

    add basic get endpoint in API, with cache

---
+++ src/transcript_api.tsx
@@ -0,0 +1,29 @@
+import { IncomingMessage } from "http";
+import { get } from "https"
+import axios, { AxiosResponse } from 'axios';
+
+const ls = require('localstorage-ttl');
+
+export default class AssemblyAiClient {
+    private key: string;
+
+    constructor(key: string) {
+        this.key = key
+    }
+
+    async get_transcript(id: string, callback: (data: any, status: number) => void ) {
+        const data = ls.get(id)
+        if (data != undefined) {
+            callback(data, 200)
+            return 
+        } 
+
+        axios.get('https://api.assemblyai.com/v2/transcript/' + id, {
+            headers: {authorization: this.key}
+          }).then((v: AxiosResponse) => {
+            const {data, status} = v
+            ls.set(id, data, [20 * 1000]);
+            callback(data, status)
+          })
+    }
+}
